ebaf4f9edaa8d78c8b5aa756012492b1aa708c9f
Reinaldo Vianna <reinaldo.vns@gmail.com>
2019-05-21 16:36:07 -0300

    reversal of previous implementation, keeping the new rules

--- app/models/food.rb
+++ app/models/food.rb
@@ -12,30 +12,27 @@ class Food
 
   def cook_calculate
     return self.result_calculator = time_cook if [time_hourglass_one, time_hourglass_two].include?(time_cook)
-    th = { 
-          one: { value: time_hourglass_one.to_i, sum: time_hourglass_one.to_i},
-          two: { value: time_hourglass_two.to_i, sum: time_hourglass_two.to_i}
-         }
-
+    time_min_max =[time_hourglass_one.to_i, time_hourglass_two.to_i].minmax
+    time_min_max_sum = time_min_max.clone
+    
     begin
-      Timeout.timeout(3) do
-        while((th[:one][:sum] - th[:two][:sum]).abs != time_cook.to_i) do
-          if(th[:one][:sum] < th[:two][:sum])
-            th[:one][:sum] += th[:one][:value]
-          elsif(th[:one][:sum] > th[:two][:sum])
-            th[:two][:sum] += th[:two][:value]
-          else 
-            if(time_cook.to_i % th[:one][:value] == 0 ||
-               time_cook.to_i % th[:two][:value] == 0)
+      Timeout.timeout(5) do
+        while (time_min_max_sum.max - time_min_max_sum.min) != time_cook.to_i do
+          if (time_min_max_sum.first < time_min_max_sum.last)
+            time_min_max_sum[0] = time_min_max_sum.first + time_min_max.first
+          elsif (time_min_max_sum.first > time_min_max_sum.last)
+            time_min_max_sum[1] = time_min_max_sum.last + time_min_max.last
+          else
+            if(time_cook.to_i % time_min_max.min == 0 ||
+               time_cook.to_i % time_min_max.max == 0)
               return self.result_calculator = time_cook.to_i
             end
 
             raise "Não é possível calcular o tempo de preparo com as ampulhetas informadas."
           end
 
-          current_time_min = [th[:one][:sum], th[:two][:sum]].min
-          if(current_time_min == time_cook.to_i)
-            return self.result_calculator =  current_time_min
+					if(time_min_max_sum.min == time_cook.to_i)
+            return self.result_calculator =  time_min_max_sum.min
           end
 
         end
@@ -43,6 +40,7 @@ class Food
     rescue Timeout::Error
       raise "Tempo de processamento excedido."
     end
-    self.result_calculator = [th[:one][:sum], th[:two][:sum]].max
+
+    self.result_calculator = time_min_max_sum.max
   end
 end
